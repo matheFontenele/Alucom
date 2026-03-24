9453d5c1dcfb4d5c595bf28d66328aec21c3ea63
Matheus Fontenele <matheusfontene979700@gmail.com>
2026-03-24 19:58:19 -0300

    atualmoasdrequisicoesss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,6 +54,11 @@
                 <i class="ph ph-arrows-clockwise text-xl"></i> Movimentações
             </a>
 
+            <a href="{{ route('requisicoes.index') }}"
+                class="flex items-center gap-3 p-3 rounded-lg transition {{ request()->routeIs('requisicoes.*') ? 'bg-red-600 text-white shadow-lg shadow-red-900/20' : 'hover:bg-slate-800 hover:text-white text-slate-300' }}">
+                <i class="ph-box-arrow-down text-xl"></i> Requicisões
+            </a>
+
             <a href="{{ route('catalogos.index') }}"
                 class="flex items-center gap-3 p-3 rounded-lg transition {{ request()->routeIs('catalogos.*') ? 'bg-red-600 text-white shadow-lg shadow-red-900/20' : 'hover:bg-slate-800 hover:text-white text-slate-300' }}">
                 <i class="ph ph-book-open-text text-xl"></i> Catalogo
