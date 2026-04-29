32e9034f0d664b6e1b7535531481bcc2e368bdfc
Matheus <matheusfontenele979700@gmail.com>
2026-04-29 15:57:19 -0300

    atualização layout2

--- app/Models/GuiaComputador.php
+++ app/Models/GuiaComputador.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class GuiaComputador extends Model
 {
+
+    protected $table = 'guia_computadores';
     protected $fillable = ['fabricante', 'marca_modelo', 'foto', 'processador', 'memoria', 'armazenamento', 'geracao', 'obs'];
 }
